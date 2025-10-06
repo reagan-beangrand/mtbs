f6db42adf8f9cd01218bef140f5ed5ceb0bf664d
reagan-beangrand <reagan.grand@outlook.com>
2025-10-06 15:50:10 +0530

    feat: crm custom fields

--- mtbs/hooks.py
+++ mtbs/hooks.py
@@ -247,7 +247,7 @@ fixtures =[
     #{"dt":"MTBS EventType"},
     #{"dt":"MTBS CourseName"}
     #{"dt":"CRM Lead Source","filters":[["name","IN",["API"]]]},
-    {"dt":"Property Setter","filters":[["module","=","MTBS"]]},
+    #{"dt":"Property Setter","filters":[["module","=","MTBS"]]},
     #{"dt":"Custom Field","filters":[["module","=","MTBS"]]},    
     #{"dt":"CRM Fields Layout","filters":[["name","IN",["CRM Deal-Data Fields","CRM Lead-Data Fields","CRM Deal-Side Panel","CRM Lead-Side Panel","CRM Lead-Quick Entry","CRM Deal-Required Fields"]]]}
 ]
